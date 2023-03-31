e2699b517885c7c51b0fddca07f0341941307cf4
jianghx <jianghx@youyuan.com>
2023-03-31 13:56:45 +0800

    init 8

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -73,7 +73,7 @@ function formatDate(date) {
     const hours = ('0' + date.getHours()).slice(-2);
     const minutes = ('0' + date.getMinutes()).slice(-2);
     const seconds = ('0' + date.getSeconds()).slice(-2);
-    return `${year}${month}${day}${hours}${minutes}${seconds}`;
+    return `${year}-${month}-${day}_${hours}:${minutes}:${seconds}`;
 }
 
 window.addEventListener("load", function () {
