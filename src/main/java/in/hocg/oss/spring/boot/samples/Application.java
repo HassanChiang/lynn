1f596df63c6aa47a9f4fd4b0c41ab4275724b0dc
jianghx <jianghx@youyuan.com>
2023-03-31 10:22:26 +0800

    init 3

--- src/main/java/in/hocg/oss/spring/boot/samples/Application.java
+++ src/main/java/in/hocg/oss/spring/boot/samples/Application.java
@@ -3,10 +3,6 @@ package in.hocg.oss.spring.boot.samples;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @author hocgin
- * @date 2019/6/13
- */
 @SpringBootApplication
 public class Application {
 
